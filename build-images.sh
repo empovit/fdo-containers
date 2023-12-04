cb1aa1b1590aa1d4e5184d0470b03db405af0d4f
Vitaliy Emporopulo <vemporop@redhat.com>
2023-12-04 19:25:19 +0200

    Prevent from upgrading OS

--- build-images.sh
+++ build-images.sh
@@ -7,8 +7,8 @@ for f in images/Containerfile.*
 do
     name=${f##$prefix}
     podman build \
-		  --secret id=rhsm_org,src=${RHSM_ORG_FILE} \
-		  --secret id=rhsm_activationkey,src=${RHSM_ACTIVATIONKEY_FILE} \
-      --build-arg RHEL_VERSION=${RHEL_VERSION} \
-      -t "fdo-${name}:rhel${RHEL_VERSION}" -f $f images/
+        --secret id=rhsm_org,src=${RHSM_ORG_FILE} \
+        --secret id=rhsm_activationkey,src=${RHSM_ACTIVATIONKEY_FILE} \
+        --build-arg RHEL_VERSION=${RHEL_VERSION} \
+        -t "fdo-${name}:rhel${RHEL_VERSION}" -f $f images/
 done
