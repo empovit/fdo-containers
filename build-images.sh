eaea90339b50f3809ef8713c74949890d4b98215
Vitaliy Emporopulo <vemporop@redhat.com>
2023-12-12 12:46:11 +0200

    Make container subscription manager configurable

--- build-images.sh
+++ build-images.sh
@@ -2,6 +2,7 @@
 
 RHEL_VERSION=${RHEL_VERSION:-9.3}
 IMAGE_REPO=${IMAGE_REPO:-"quay.io/vemporop"}
+FORCE_CONTAINER_SUBSCRIPTION_MANAGER=${FORCE_CONTAINER_SUBSCRIPTION_MANAGER:-0}
 prefix="images/Containerfile."
 
 for f in images/Containerfile.*
@@ -11,5 +12,6 @@ do
         --secret id=rhsm_org,src=${RHSM_ORG_FILE} \
         --secret id=rhsm_activationkey,src=${RHSM_ACTIVATIONKEY_FILE} \
         --build-arg RHEL_VERSION=${RHEL_VERSION} \
+        --build-arg FORCE_CONTAINER_SUBSCRIPTION_MANAGER=${FORCE_CONTAINER_SUBSCRIPTION_MANAGER} \
         -t "${IMAGE_REPO}/fdo-${name}:rhel${RHEL_VERSION}" -f $f images/
 done
